ba3c47b7abcfbd1319d1354f0d8a379f0ad998b1
AkihiroAck <canpolad99@gmail.com>
2025-09-21 23:39:26 +0500

    2025-09-21

--- backend/entrypoint_web.sh
+++ backend/entrypoint_web.sh
@@ -2,5 +2,5 @@
 cd ./scaw
 python manage.py makemigrations
 python manage.py migrate
-python manage.py collectstatic --noinput --clear
+python manage.py collectstatic --noinput
 gunicorn scaw.wsgi:application --bind 0.0.0.0:8000
